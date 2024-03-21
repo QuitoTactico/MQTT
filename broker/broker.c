d1b6130239c18b3070d937f1dcc7546caee7a5b7
MiguelCock <macockc@eafit.edu.co>
2024-03-21 10:07:19 -0500

    base socket for connection done

--- broker/broker.c
+++ broker/broker.c
@@ -2,23 +2,74 @@
 
 int main(int argc, char *argv[])
 {
-    int sockfd; //socket file descriptor
     struct addrinfo hints, *res;
+    int sockfd;
 
     // first, load up address structs with getaddrinfo():
-    memset(&hints, 0, sizeof(hints));
-    hints.ai_family = AF_UNSPEC; // use IPv4 or IPv6, whichever
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
-    getaddrinfo(NULL, "3490", &hints, &res);
 
-    // make a socket:
+    memset(&hints, 0, sizeof hints);
+    hints.ai_family = AF_INET; // use IPv4 
+    hints.ai_socktype = SOCK_STREAM; // TCP
+    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+
+    if (getaddrinfo(NULL, MYPORT, &hints, &res) != 0)
+    {
+        perror("GET ADDRES INFO");
+        return 1;
+    }
+
     sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
-    
-    // bind it to the port we passed in to getaddrinfo():
-    bind(sockfd, res->ai_addr, res->ai_addrlen);
+
+    if (sockfd == -1)
+    {
+        perror("SOCKET");
+        return 1;
+    }
+
+    if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0)
+    {
+        perror("BINDING");
+        return 1;
+    }
+
+    /*
+    if (connect(sockfd, res->ai_addr, res->ai_addrlen) < 0)
+    {
+        perror("CONNECTING");
+        return 1;
+    }
+    */
+
+    if (listen(sockfd, QUEUESIZE) != 0)
+    {
+        perror("LISTENING");
+        return 1;
+    }
+
+    struct sockaddr_storage their_addr;
+    socklen_t addr_size = sizeof(their_addr);
+
+    int newSocketfd = accept(sockfd, (struct sockaddr *)&their_addr, &addr_size);
+
+    if (newSocketfd == -1)
+    {
+        perror("ACCEPT");
+        return 1;
+    }
+
+    printf("conection accecpted\n");
+
+    /*
+    char buf[1000];
+    int len = 1000;
+
+    recv(newSocketfd, buf, len, 0);
+    */
+
+    close(sockfd);
+    close(newSocketfd);
+
+    freeaddrinfo(res);
 
     return 0;
 }
-
-//coment
