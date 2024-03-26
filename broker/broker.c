35e5f5cf9fac8644366f6a8cd850f21de44fac62
MiguelCock <macockc@eafit.edu.co>
2024-03-25 19:16:55 -0500

    organice the main function and encapsulate functionalities

--- broker/broker.c
+++ broker/broker.c
@@ -2,50 +2,16 @@
 
 int main(int argc, char *argv[])
 {
-    struct addrinfo hints, *res;
-    int sockfd;
-
-    // first, load up address structs with getaddrinfo():
-
-    memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; // use IPv4 
-    hints.ai_socktype = SOCK_STREAM; // TCP
-    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
-
-    if (getaddrinfo(MY_IP, MYPORT, &hints, &res) != 0)
-    {
-        perror("GET ADDRES INFO");
-        return 1;
-    }
-
-    // define the socket file descriptor to listen for incomming connections
-
-    sockfd = socket(res->ai_family, res->ai_socktype, res->ai_protocol);
+    // create the socket file descriptor with the ip, port and queue size
+    int sockfd = createSocket(NULL, MY_PORT, QUEUESIZE);
 
     if (sockfd == -1)
     {
-        perror("SOCKET");
-        return 1;
-    }
-
-    // bind the socketfd to the port
-
-    if (bind(sockfd, res->ai_addr, res->ai_addrlen) != 0)
-    {
-        perror("BINDING");
-        return 1;
-    }
-
-    // define the size of the queue to listen for incomming connections
-
-    if (listen(sockfd, QUEUESIZE) != 0)
-    {
-        perror("LISTENING");
+        close(sockfd);
         return 1;
     }
 
     // accept one connection and write the info of the connected client and create its file descriptor
-
     struct sockaddr_storage their_addr;
     socklen_t addr_size = sizeof(their_addr);
 
@@ -58,26 +24,13 @@ int main(int argc, char *argv[])
     }
 
     printf("conection accecpted\n\n");
-
-    const int NAMESIZE = 100;
-    char hostName[NAMESIZE];
-    memset(&hostName, 0, NAMESIZE);
-    char serviceName[NAMESIZE];
-    memset(&serviceName, 0, NAMESIZE);
-    int flags = NI_NUMERICHOST + NI_NUMERICSERV;
-
-    if (getnameinfo((struct sockaddr *)&their_addr, addr_size, hostName, NAMESIZE, serviceName, NAMESIZE, flags) != 0)
-    {
-        perror("GET NAME INFO");
-        return 1;
-    }
-
-    printf("ip conection from %s to %s\n", hostName, serviceName);
+    
+    // prints the info of the server socket and the client socket
+    printSocketInfo(sockfd, clientfd, &their_addr, addr_size);
 
     // print the info recieved by the client 
-
     char buf[1000];
-    int len = 1000;
+    int len = 1000; 
 
     while(buf[0] != 'q')
     {
@@ -85,13 +38,15 @@ int main(int argc, char *argv[])
 
         printf("info del cliente:\n\n%s\n\n", buf);
 
-        memset(&buf, 0, len);
+        if(buf[0] != 'q')
+        {
+            memset(&buf, 0, len);
+        }
     }
+    memset(&buf, 0, len);
     
     close(sockfd);
     close(clientfd);
-
-    freeaddrinfo(res);
 
     return 0;
 }
