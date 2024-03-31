35dc886533c8ac4ee2d94fe325ea4279e8a58cee
Jonathanbees <jonathanbetancurespinosaq@gmail.com>
2024-03-31 01:57:36 -0500

    connect message

--- client/client.c
+++ client/client.c
@@ -1,26 +1,25 @@
-#include "mqttServer.h"
-    #include <stdlib.h>
+#include "mqttClient.h"
+#include <stdlib.h>
 
 int main(int argc, char *argv[])
 {
     struct addrinfo hints, *res;
 
+    connectVariableHeader connectVarHeader; // Initialize the instance of the variableHeader struct in the stack
+    // payload
 
-    struct connectVariableHeader* connectVarHeader = (connectVariableHeader*)malloc(sizeof(struct connectVariableHeader)); // Allocate memory using malloc and cast the result to the appropriate type
-    //payload
-
-    struct connectPayload* connectPayload = (struct connectPayload*)malloc(sizeof(struct connectPayload)); // Allocate memory using malloc and cast the result to the appropriate type;
+    connectPayload myconnectPayload; // Initialize the instance of the payload struct in the stack
 
     int sockfd;
 
     // first, load up address structs with getaddrinfo():
 
     memset(&hints, 0, sizeof hints);
-    hints.ai_family = AF_INET; // use IPv4 
+    hints.ai_family = AF_INET;       // use IPv4
     hints.ai_socktype = SOCK_STREAM; // TCP
-    hints.ai_flags = AI_PASSIVE; // fill in my IP for me
+    hints.ai_flags = AI_PASSIVE;     // fill in my IP for me
 
-    if (getaddrinfo(NULL, MYPORT, &hints, &res) != 0)
+    if (getaddrinfo(NULL, MY_PORT, &hints, &res) != 0)
     {
         perror("GET ADDRES INFO");
         return 1;
@@ -52,62 +51,176 @@ int main(int argc, char *argv[])
 
     printf("conection accecpted\n");
 
-    // for cycle to ask the user the action that wants to do, like: select: connect, publish, subscribe, etc.
-    char answer[1000];
+    char answer[20];
     int len = 1000;
+    char message[500];
+    char anssession[50];
+
+    char uniqueID[23];
+    uint16_t len_uniqueID;
+
+    char answerWill[20];
+    // undefined lengths for the topic, message and password and user
+    uint16_t len_topic; // Pointer to hold the length as a string
+    char *topic = NULL;
+    size_t longitud = 0;
+    ssize_t readtopic;
+
+    char *messagewill = NULL;
+    ssize_t readmessage;
+    uint16_t len_message;
+
+    char *password = NULL;
+    ssize_t readpassword;
+    uint16_t len_pass;
+
+    char *username = NULL;
+    ssize_t readuser;
+    uint16_t len_user;
 
     printf("select: connect, publish, subscribe, disconnect, exit\n");
     scanf("%s", answer);
-    for(;;){
-        if(answer == "connect"){
-            //Work with the instance of the variableHeader struct
+    printf("You selected: %s\n", answer);
 
-            connectVarHeader->connectFlags = 1;
+    for (;;)
+    {
 
+        if (strcmp(answer, "connect") == 0)
+        {
+            // In the first three bytes there´s reserved space for the fixed header
+            message[3] = 0;
+            message[4] = 4;
+            message[5] = 'M';
+            message[6] = 'Q';
+            message[7] = 'T';
+            message[8] = 'T';
+            message[9] = 4;
+            printf("Do you have a created session?: ");
+            scanf("%s", anssession);
+            // the user dont have a session, so, the server must create one and then, store it in the server to the next time the user connects
+            if (strcmp(anssession, "0") == 0)
+            {
+                printf("ID:");
+                scanf("%s", uniqueID);
+                len_uniqueID = strlen(uniqueID);
+                getchar();                                   // Consume the newline character that´s still in the buffer from the previous scanf
+                strcpy(&message[10], (char *)&len_uniqueID); // Concatenate the length to the message
+                strcat(&message[10], uniqueID);              // Concatenate the uniqueID to the message
+                printf("The length of the ID is: %zu\n", strlen(uniqueID));
+                // printf("The message is: %s\n", &message[10]); // Testing
 
+                printf("Do you want to leave the will flag?:");
+                scanf("%s", answerWill);
+                if (strcmp(answerWill, "0") == 0)
+                {
+                    printf("Will topic:");
+                    getchar(); // Consume the newline character that´s still in the buffer from the previous scanf
+                    readtopic = getline(&topic, &longitud, stdin);
 
+                    if (readtopic != -1)
+                    {
+                        printf("You entered: %s", topic);
+                        printf("The length of the message is: %zu\n", strlen(topic));
+                        topic[strcspn(topic, "\n")] = 0; // Remove the newline character from the topic
+                        len_topic = longitud;            // Pointer to hold the length as a string
+                        // sprintf(len_topic, "%zu", strlen(topic));               // Convert the length to a string
+                        strcat(&message[10], (char *)&len_topic); // Concatenate the length to the message
+                        strcat(&message[10], topic);              // Concatenate the topic to the message
+                        // free(topic);
+                    }
+                    printf("Will message:");
+                    readmessage = getline(&messagewill, &longitud, stdin); // the getline functions expects a pointer to a char pointer and a pointer to a size_t variable, that´s the reason why we use the & operator
+
+                    if (readmessage != -1)
+                    {
+                        printf("You entered: %s", messagewill);
+                        printf("The length of the message is: %zu\n", strlen(messagewill));
+                        messagewill[strcspn(messagewill, "\n")] = 0;
+                        len_message = longitud;
+                        strcat(&message[10], (char *)&len_message); // Concatenate the length to the message
+                        strcat(&message[10], messagewill);          // Concatenate the message to the message
+                        // free(messagewill);
+                    }
+
+                    printf("Username:");
+                    readuser = getline(&username, &longitud, stdin); // Read the password
+
+                    if (readuser != -1)
+                    {
+                        printf("You entered the username: %s", username);
+                        printf("The length of the username is: %zu\n", strlen(username) - 1);
+                        username[strcspn(username, "\n")] = 0;
+                        len_user = longitud;
+
+                        strcat(&message[10], (char *)&len_user); // Concatenate the length to the message
+                        strcat(&message[10], username);          // Concatenate the message to the message
+                        free(username);
+                    }
+
+                    printf("Password:");
+                    readpassword = getline(&password, &longitud, stdin); // Read the password
+
+                    if (readpassword != -1)
+                    {
+                        printf("You entered the password: %s", password);
+                        printf("The length of the password is: %zu\n", strlen(password));
+                        password[strcspn(password, "\n")] = 0; // Remove the newline character from the password
+                        len_pass = longitud;
+
+                        strcat(&message[10], (char *)&len_pass); // Concatenate the length to the message
+                        strcat(&message[10], password);          // Concatenate the message to the message
+                        free(password);
+                    }
+                    // concatenate the topic and the message to the message variable
+                    printf("The message is: %s", &message[10]); // Testing
+                    break;
+                }
+                else
+                {
+                    // that is the section that the server must discard any previous session information
+                    strcat(&message[10], "00");
+                }
+                break;
+            }
             // send the connect message to the server
-            char* connectMessage = "CONNECT";
-            send(sockfd, connectMessage, strlen(connectMessage), 0);
+            send(sockfd, message, strlen(message), 0);
             printf("connect message sent\n");
+            free(topic);
+            free(messagewill);
         }
-        else if(answer == "publish"){
+
+        else if (answer == "publish")
+        {
             // send the publish message to the server
-            char* publishMessage = "PUBLISH";
+            char *publishMessage = "PUBLISH";
             send(sockfd, publishMessage, strlen(publishMessage), 0);
             printf("publish message sent\n");
         }
-        else if(answer == "subscribe"){
+        else if (answer == "subscribe")
+        {
             // send the subscribe message to the server
-            char* subscribeMessage = "SUBSCRIBE";
+            char *subscribeMessage = "SUBSCRIBE";
             send(sockfd, subscribeMessage, strlen(subscribeMessage), 0);
             printf("subscribe message sent\n");
         }
-        else if(answer == "disconnect"){
+        else if (answer == "disconnect")
+        {
             // send the disconnect message to the server
-            char* disconnectMessage = "DISCONNECT";
+            char *disconnectMessage = "DISCONNECT";
             send(sockfd, disconnectMessage, strlen(disconnectMessage), 0);
             printf("disconnect message sent\n");
         }
-        else if(answer == "exit"){
+        else if (answer == "exit")
+        {
             // send the exit message to the server
-            char* exitMessage = "EXIT";
+            char *exitMessage = "EXIT";
             send(sockfd, exitMessage, strlen(exitMessage), 0);
             printf("exit message sent\n");
             break;
-
-
-
         }
-
-
-
     }
-
-
-
-
-
+    free(topic);
+    free(messagewill);
 
     close(sockfd);
 
---
+++ client/mqttClient.h
@@ -0,0 +1,259 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <string.h>
+#include <threads.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+#define MY_PORT "1883"
+#define MY_IP "192.168.10.13"
+#define QUEUESIZE 10
+
+//================================================================================================================
+
+/*
+    ====FIXE HEADER====
+
+    it is always in there
+
+    ====VARIABLE HEADER====
+
+    PUBLISH
+    PUBACK
+    PUBREC
+    PUBREL
+    PUBCOMP
+    SUBSCRIBE
+    SUBACK
+    UNSUBSCRIBE
+    UNSUBACK
+
+    ====PAYLOAD====
+
+    CONNECT     required
+    PUBLISH     optional
+    SUBSCRIBE   required
+    SUBACK      required
+    UNSUBSCRIBE required
+    UNSUBACK    required
+*/
+
+//================================================================================================================
+
+/*******************************************/
+/*                                         */
+/*               FIXED HEADER              */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint8_t messageType;
+    uint16_t remainingLenght;
+} fixedHeader;
+
+/*******************************************/
+/*                                         */
+/*              CONTROL PACKET             */
+/*                                         */
+/*******************************************/
+
+#define CONNECT   0b00010000   // 1 || CONNECT     || CLIENT TO SERVER //
+#define CONNACK   0b00100000   // 2 || CONNECT ACK || SERVER TO CLIENT
+#define PUBLISH   0b00110000   // 3 || PUBLISH MESSAGE || BOTH WAYS
+#define PUBACK    0b01000000   // 4 || PUBLISH ACK     || BOTH WAYS
+
+// WILL NOT IMPLETEMT FOR NOW
+#define PUBREC    0b01010000   // 5 || PUBLISH RECIEVE  || BOTH WAYS
+#define PUBREL    0b01100000   // 6 || PUBLISH RELEASE  || BOTH WAYS
+#define PUBCOMP   0b01110000   // 7 || PUBLISH COMPLETE || BOTH WAYS
+//************
+
+#define SUBSCRIBE 0b10000000   // 8 || SUBSCRIBE REQUEST || CLIENT TO SERVER
+#define SUBACK    0b10010000   // 9 || SUBSCRIBE ACK     || SERVER TO CLIENT
+
+// WILL NOT IMPLETEMT FOR NOW
+#define UNSUBSCRIBE  0b10100000   // 10 || UNSUBSCRIBE REQUEST || CLIENT TO SERVER
+#define UNSUBACK     0b10110000   // 11 || UNSUBSCRIBE ACK     || SERVER TO CLIENT
+#define PINGREQ      0b11000000   // 12 || PING REQUEST  || CLIENT TO SERVER
+#define PINGRESP     0b11010000   // 13 || PING RESPONSE || SERVER TO CLIENT
+//************
+
+#define DISCONNECT   0b11100000   // 14 || CLIENT IS DISCONNECTING || BOTH WAYS
+#define AUTH         0b11110000   // 15 || AUTENTICATION EXCHANGE  || BOTH WAYS
+
+/*******************************************/
+/*                                         */
+/*                  FLAGS                  */
+/*                                         */
+/*******************************************/
+
+#define DUP    0b00000001 // DUPLICATE DELIVERY PUBLISH
+#define QOS    0b00000110 // PUBLISH CUALITY OF SERVICE
+#define RETAIN 0b00001000 // PUBLISH RETEINED MESSAGE FLAG
+
+
+//================================================================================================================
+
+/*******************************************/
+/*                                         */
+/*         CONNECT VARIABLE HEADER         */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    // 4
+    uint16_t nameLenght;
+    // "MQTT"
+    char name[4];
+    // 4 for 3.1 | 5 for 5
+    uint8_t version;
+    // type of connection
+    uint8_t connectFlags;
+    // the time in seconds between each user mqtt packet transmition
+    uint16_t keepAlive;
+} connectVariableHeader;
+
+/*******************************************/
+/*                                         */
+/*              CONNECT FLAGS              */
+/*                                         */
+/*******************************************/
+
+// 1 for new session 0 for existing sessing if there are no previus sessions
+#define CLEANSTART  0b00000010
+// 1 if the client wants to send others a message of a unespected disconection
+#define WILLFLAG    0b00000100
+// 1 | 2 | 3 depending on the level of assuranse that the user wants if the will flag is set to 1
+#define WILLQOS     0b00011000
+// if 1 the server must return the message as a retainable message
+#define WILLRETAIN  0b00100000
+// if set to 1 the payload has the password
+#define PASSWORD    0b01000000
+// if set to 1 the payload has the username
+#define USERNAME    0b10000000
+
+/*******************************************/
+/*                                         */
+/*                PROPERTY                 */
+/*                                   MQTT 5*/
+/*******************************************/
+/*
+typedef struct {
+    // the size of information of the property
+    uint8_t propertiesLenght;
+    // type of property
+    uint8_t property;
+    // information of property
+    uint8_t propertyData;
+} connectVariableHeaderProperties;
+
+//
+#define SESSIONEXPIRING 0x11 // 17
+//
+#define RECIEVEMAXIMUM 0x21 // 21
+//
+#define MAXIMUNPACKETSIZE 0x27 // 29
+//
+#define TOPICALIASMAXIMU 0x22 // 34
+//
+#define REQUESTRESPONSEI 0x19 // 25
+//
+#define REQUESTPROBLEM 0x17 //23
+//
+#define USERPROPERTY 0x26 //38
+//
+#define AUTENTICATIONMETHOD 0x15 // 21
+//
+#define AUTENTICATIONDATA 0x16 //22
+*/
+
+/*******************************************/
+/*                                         */
+/*             CONNECT PAYLOAD             */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint16_t clientIDSize;
+    char* clientID;
+    uint16_t willTopicSize;
+    char* willTopic;
+    uint16_t willMessageSize;
+    char* willMessage;
+    uint16_t userNameSize;
+    char* userName;
+    uint16_t passWordSize;
+    char* passWord;
+} connectPayload;
+
+//================================================================================================================
+
+/*******************************************/
+/*                                         */
+/*         PUBLISH VARIABLE HEADER         */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint16_t topicSize;
+    char* topic;
+    uint16_t identifier;
+} publishVariableHeader;
+
+
+/*******************************************/
+/*                                         */
+/*             PUBLISH PAYLOAD             */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint16_t payloadSize;
+    char* data;
+} publishPayload;
+
+//================================================================================================================
+
+/*******************************************/
+/*                                         */
+/*        SUBSCRIBE VARIABLE HEADER        */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint16_t identifier;
+} subscribeVariableHeader;
+
+/*******************************************/
+/*                                         */
+/*            SUBSCRIBE PAYLOAD            */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    uint16_t payloadSize;
+    char* topic;
+    uint8_t qos;
+} subscribePayload;
+
+//================================================================================================================
+
+/*******************************************/
+/*                                         */
+/*                 SOCKET                  */
+/*                                         */
+/*******************************************/
+
+typedef struct {
+    int sockfd;
+    int clientfd;
+} SOCKET;
+
+// create the socket file descriptor with the ip, port and queue size
+int createSocket(char* port, char* ip, int queue);
+
+// prints the info of the server socket and the client socket
+int printSocketInfo(int sockfd, int clientfd, struct sockaddr_storage* their_addr, socklen_t addr_size);
+
+int handleRequest(void *args);
